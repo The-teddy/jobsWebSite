8929523a5a7e6eccd72b8e91a9115c1815f0b8d6
Teddy <augustomarcio919@gmail.com>
2022-07-11 17:15:36 -0300

    jobsWeb

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require ('express')
 const app = express()
-const porta = process.env.PORT 
+const porta = process.env.PORT || 8080
 
 
 app.use(express.static(__dirname + '/dist/'))
